a40d7732534271a5251c8b804830a2cdb27cb50a
amanshr07 <42678955+amanshr07@users.noreply.github.com>
2021-02-13 19:14:35 +0530

    Twopeoplemeet

---
+++ 0)twopeoplemeet.py
@@ -0,0 +1,34 @@
+
+# coding: utf-8
+
+# In[8]:
+
+
+def Meet(x1, x2, v1, v2):
+    if x1>x2 and v1>v1:
+        return False
+    if x2>x1 and v2>v1:
+        return False
+        while x1 >= x2:  
+      
+            if x1 == x2:  
+                return True; 
+          
+
+            x1 = x1 + v1;  
+          
+        
+            x2 = x2 + v2;  
+    
+x1 = 5; 
+v1 = 8; 
+x2 = 4; 
+v2 = 7; 
+if Meet(x1, x2,v1, v2): 
+    print("Yes");  
+else: 
+    print("No"); 
+    
+
+   
+
