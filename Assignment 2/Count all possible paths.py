8e8efe7b5e55d29ca9762cf2eb9fadf86f93a0fb
amanshr07 <42678955+amanshr07@users.noreply.github.com>
2021-02-14 20:05:19 +0530

    Add files via upload

---
+++ Assignment 2/Count all possible paths.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# In[7]:
+
+
+def numberOfPaths(m, n): 
+# If either given row number is first 
+# or given column number is first 
+    if(m == 1 or n == 1): 
+        return 1
+  
+# If diagonal movements are allowed 
+# then the last addition 
+# is required. 
+    return numberOfPaths(m-1, n) + numberOfPaths(m, n-1) 
+  
+# Driver program to test above function  
+m = 3
+n = 3
+print(numberOfPaths(m, n))
+
