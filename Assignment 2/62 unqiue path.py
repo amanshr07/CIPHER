fbfe76225319ca565ba28021f46f29bc537e03a3
amanshr07 <42678955+amanshr07@users.noreply.github.com>
2021-02-15 11:53:21 +0530

    Add files via upload

---
+++ Assignment 2/62 unqiue path.py
@@ -0,0 +1,21 @@
+
+# coding: utf-8
+
+# In[10]:
+
+
+import math
+
+class Solution:
+    # @param A : integer
+    # @param B : integer
+    # @return an integer
+    def uniquePaths(self, A, B):
+        steps_right = A - 1
+        steps_down = B - 1
+        fr = math.factorial(steps_right)
+        fd = math.factorial(steps_down)
+        ft = math.factorial(steps_right + steps_down)
+        print(fr, fd, (fr + fd), (fr * fd))
+        return ft // (fr * fd)
+
