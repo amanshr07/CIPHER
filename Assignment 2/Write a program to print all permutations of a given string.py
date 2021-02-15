bb64190fbc84277eb4f14d9b5773b866da7d38c1
amanshr07 <42678955+amanshr07@users.noreply.github.com>
2021-02-15 11:37:19 +0530

    Add files via upload

---
+++ Assignment 2/Write a program to print all permutations of a given string.py
@@ -0,0 +1,24 @@
+
+# coding: utf-8
+
+# In[10]:
+
+
+def toString(List):
+   return ''.join(List)
+# permutations
+def permute(a, l, r):
+   if l == r:
+      print (toString(a))
+   else:
+      for i in range(l, r + 1):
+         a[l], a[i] = a[i], a[l]
+         permute(a, l + 1, r)
+         a[l], a[i] = a[i], a[l] # backtracking
+# main
+string = "TUT"
+n = len(string)
+a = list(string)
+print("The possible permutations are:",end="\n")
+permute(a, 0, n-1)
+
